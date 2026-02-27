7dcbde1f091f51e41e510381f618f7ada6626569
ARTEM <artem@MacBook-Air-ARTEM.local>
2026-02-27 20:32:30 +0200

    first commit

--- spotify-mini/app/layout.tsx
+++ spotify-mini/app/layout.tsx
@@ -24,10 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     <head>
-     	<script src="https://telegram.org/js/telegram-web-app.js"></script>
-     </head> 
-     <body
+      <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
