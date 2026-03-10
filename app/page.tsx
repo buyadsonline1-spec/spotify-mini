9dbd131548032e049176bd897dd3ebcb7b716b4a
ARTEM <artem@MacBook-Air-ARTEM.local>
2026-03-10 20:34:57 +0200

    fix style comma

--- app/page.tsx
+++ app/page.tsx
@@ -1777,7 +1777,7 @@ function openCurrentTrackMenu() {
           boxShadow: "0 10px 30px rgba(0,0,0,0.16)",
           color: "#fff",
           outline: "none",
-          boxSizing: "border-box",
+          
         }}
       />
 
